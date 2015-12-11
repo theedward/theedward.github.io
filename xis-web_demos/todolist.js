94b96abfc7ad167648a2f08fcb2047ce894767a3
João Eduardo Seixas <ed-01@hotmail.com>
2015-12-11 11:12:42 +0000

    Finished the example!

--- xis-web_demos/todolist.js
+++ xis-web_demos/todolist.js
@@ -1,67 +1,72 @@
 $(function() {
 	var todolist = {}
-	todolist.init = {}
-	todolist.init.db = {}
+	todolist.db = {}
 	
 	// Holding database instance inside a global variable
-	todolist.init.open = function(){
-		todolist.init.db = openDatabase("todolist","1.0","TODOList",5 * 1024 * 1024);
+	todolist.open = function(){
+		todolist.db = openDatabase("todolist","1.0","TODOList",5 * 1024 * 1024);
 	}
 	
-	todolist.init.createTable = function(){
-		var database = todolist.init.db;
+	//Will create a table if it doesn't exist already
+	todolist.createTable = function(){
+		var database = todolist.db;
 		database.transaction(function(tx){
 			tx.executeSql("CREATE TABLE IF NOT EXISTS todo_list(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, content TEXT)", []);
 		});
 	}
 	
-	todolist.init.getAllTodoItems = function(renderFunc) {
-	  var database = todolist.init.db;
+	//Queries the database to get all elements and calls a function -> renderFunc on the set
+	todolist.getAllTodoItems = function(renderFunc) {
+	  var database = todolist.db;
 	  database.transaction(function(tx) {
 	    tx.executeSql("SELECT * FROM todo_list", [], renderFunc,
-	        todolist.init.onError);
+	        todolist.onError);
 	  });
 	}
 	
-	todolist.init.onError = function(tx, e) {
+	//Asks for user confirmation and if positive, drops the table and refreshes the page
+	todolist.dropTable = function(){
+		if (confirm('Queres mesmo apagar TODAS as notas?')) {
+			// Drop Table!
+    		var database = todolist.db;
+			database.transaction(function(tx){
+			tx.executeSql("DROP TABLE todo_list", []);
+			});
+			
+			location.reload();
+		} else {
+		    // Do nothing!
+		}	
+	}
+	
+	//Function to be called when database queries fail
+	todolist.onError = function(tx, e) {
 	  alert("There has been an error: " + e.message);
 	}
 	
-	todolist.init.onSuccess = function(tx, r) {
+	//Function to be called when database queries succeeed. TODO: Add a toastr notification
+	todolist.onSuccess = function(tx, r) {
 	  // re-render the data.
 	  // loadTodoItems is defined in Step 4a
 	}
 	
-	
-	
+	//The "main" function
 	function init(){
 		if(typeof(openDatabase) !== 'undefined')
 		{
-			todolist.init.open();
-			todolist.init.createTable();
-			todolist.init.getAllTodoItems(loadTodoItems);
+			todolist.open();
+			todolist.createTable();
+			todolist.getAllTodoItems(hideDeleteAllButton);
+			todolist.getAllTodoItems(loadTodoItems);
 		}
 		else
 		{
 			alert('Your browser does not support webSql');
 		}
-	}
-	
-	init();
-	
-	// passing values as parameters
-	todolist.init.addTodo = function(name,content){
-		var database = todolist.init.db;
-		database.transaction(function(tx){
-			tx.executeSql("INSERT INTO todo_list(name, content) VALUES (?,?)",
-			[name, content],
-			todolist.init.onSuccess,
-			todolist.init.onError);
-			});
-	}
+	}init();
 	
 	
-	
+	//Iterate through the set and call the function that knows how to print the Todo on each element
 	function loadTodoItems(tx, rs) {
 	  var todoItems = document.getElementById("ol_todo_list");
 	  for (var i=0; i < rs.rows.length; i++) {
@@ -69,39 +74,34 @@ $(function() {
 	  }
 	}
 	
+	//If there are no Todos hide the DeleteAll button
+	function hideDeleteAllButton(tx, rs) {
+		if(rs.rows.length == 0){
+			$('#btn_delete_all').hide();
+		}
+	}
+	
+	//Find the ol, each Todo is an <li> surrounded by an <a> with a <span> with the id inside
 	function renderTodo(row, ol) {
+		var a = document.createElement('a');
 		var li = document.createElement('li');
 		var span = document.createElement('span');
-		ol.appendChild(li);
+		
+		ol.appendChild(a);
+		a.appendChild(li);
 		li.appendChild(span);
+		
+		a.setAttribute('href', "todolist_detail.html?name=" + row.name + "&content=" + row.content + "&id=" + row.id);
+		a.setAttribute('class', 'list-group-item')
 		li.setAttribute('class','list-group-item');
 		span.setAttribute('class', 'badge')
 		span.innerHTML = span.innerHTML + row.id;
         li.innerHTML=li.innerHTML + row.name + ": " + row.content;
 	}
 	
-	todolist.init.deleteTodo = function(id) {
-	  var database = todolist.init.db;
-	  database.transaction(function(tx){
-	    tx.executeSql("DELETE FROM todo_list WHERE ID=?", [id],
-	        todolist.init.onSuccess,
-	        todolist.init.onError);
-	    });
-	}
-	
-
-	$('#btn_create_todo').click(function(){
-		var todo_name = $('#name').val();
-		var todo_content = $('#content').val();
-		
-		if(todo_name.length == '' || todo_content.length == '')
-		{
-			alert('Ambos os campos são obrigatórios');
-		}
-		else
-		{
-			todolist.init.addTodo(todo_name,todo_content);
-		}
+	//OnClick listener for DeleteAll button
+	$('#btn_delete_all').click(function(){
+		todolist.dropTable();
 	});
 });
 
---
+++ xis-web_demos/todolist_detail.js
@@ -0,0 +1,134 @@
+$(function() {
+	var todolist = {}
+	todolist.init = {}
+	todolist.db = {}
+	
+	// Holding database instance inside a global variable
+	todolist.open = function(){
+		todolist.db = openDatabase("todolist","1.0","TODOList",5 * 1024 * 1024);
+	}
+	
+	//Will create a table if it doesn't exist already
+	todolist.createTable = function(){
+		var database = todolist.db;
+		database.transaction(function(tx){
+			tx.executeSql("CREATE TABLE IF NOT EXISTS todo_list(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, content TEXT)", []);
+		});
+	}
+	
+	//Function to be called when database queries fail
+	todolist.onError = function(tx, e) {
+	  alert("There has been an error: " + e.message);
+	}
+	
+	//Function to be called when database queries succeeed. TODO: Add a toastr notification
+	todolist.onSuccess = function(tx, r) {
+	  // re-render the data.
+	  // loadTodoItems is defined in Step 4a
+	}
+	
+	//When page is in edit mode, fill contents, else hide Delete button
+	todolist.fillContent = function() {
+		var id = $.QueryString["id"];
+		var name = $.QueryString["name"];
+		var content = $.QueryString["content"];
+		
+		if(id > 0 && name !== 'undefined' && content !== 'undefined'){
+			$('#name').val(name);
+			$('#content').val(content);
+		}else
+		{
+			$('#btn_delete_todo').hide();
+		}
+		
+		
+	}
+	
+	//Function that reads the query string. Should be used like: var param = $.QueryString["param"]; returns the param value;
+    $.QueryString = (function(query) {
+        if (query == "") return {};
+        var parameter = {};
+        for (var i = 0; i < query.length; ++i)
+        {
+            var value=query[i].split('=');
+            if (value.length != 2) continue;
+            parameter[value[0]] = decodeURIComponent(value[1].replace(/\+/g, " "));
+        }
+        return parameter;
+    })(window.location.search.substr(1).split('&'));
+	
+	//The "main" function
+	function init(){
+		if(typeof(openDatabase) !== 'undefined')
+		{
+			todolist.open();
+			todolist.createTable();
+			todolist.fillContent();
+			
+		}
+		else
+		{
+			alert('Your browser does not support webSql');
+		}
+	}init();
+	
+	//Inserts or Updates (if exists) the Todo in the db. Passing values as parameters
+	todolist.addTodo = function(name,content){
+		var database = todolist.db;
+		var id = $.QueryString["id"];
+		
+		if(id > 0){
+			database.transaction(function(tx){
+				tx.executeSql("UPDATE todo_list SET name = ?, content=? WHERE id = ?",
+				[name, content, id],
+				todolist.onSuccess,
+				todolist.onError);
+			});
+		}else
+		{
+			database.transaction(function(tx){
+				tx.executeSql("INSERT INTO todo_list(name, content) VALUES (?,?)",
+				[name, content],
+				todolist.onSuccess,
+				todolist.onError);
+			});
+		}
+		
+	}
+	
+	//Delete Todo from database given the id
+	todolist.deleteTodo = function(id) {
+	  var database = todolist.db;
+	  database.transaction(function(tx){
+	    tx.executeSql("DELETE FROM todo_list WHERE ID=?", [id],
+	        todolist.onSuccess,
+	        todolist.onError);
+	    });
+	}
+	
+	//Create Button OnClick Listener
+	$('#btn_create_todo').click(function(){
+		var todo_name = $('#name').val();
+		var todo_content = $('#content').val();
+		
+		if(todo_name.length == '' || todo_content.length == '')
+		{
+			alert('Ambos os campos são obrigatórios');
+		}
+		else
+		{
+			todolist.addTodo(todo_name,todo_content);
+		}
+	});
+	
+	//Delete Button OnClick Listener
+	$('#btn_delete_todo').click(function(){
+		var id = $.QueryString["id"];
+		
+		if(id > 0){
+			todolist.deleteTodo(id);
+		}
+	});
+});
+
+		
